a94b0f715c4dfc4be8966b026162b28a406d0c0d
Sunny Moore <moore.sunny@gmail.com>
2022-06-11 14:44:30 -0700

    Test set_key and get_key_hash

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,17 +9,33 @@ describe Enigma do
     it "exists" do
       expect(@cipher).to be_a Enigma
     end
+    it "starts with an default string key" do
+      expect(@cipher.key).to eq("0.000000")
+    end
     it "has a stored character set" do
       expect(@cipher.char_set).to be_a Array
       expect(@cipher.char_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
         "x", "y", "z", " "])
     end
     it "can generate a random decimal float ex: 0.012345" do
-      #There should be 6 numbers after the decimal to allow for a leading or trailing zero
+      #There should be 10 numbers after the decimal to allow for a leading or trailing zero. We will use first 5
       expect(@cipher.random_number).to be_a Float
       expect(@cipher.random_number).to be < 1
       expect(@cipher.random_number).to be > 0
-      expect(@cipher.random_number.to_s.length).to be <= 8
-      expect(@cipher.random_number.to_s.length).to be >=7
+    end
+    it "can set the key variable to a string version of random number" do
+      #ex: ".01234. I want the decimal"
+      @cipher.set_key
+      expect(@cipher.key).to be_a String
+      expect(@cipher.key[0]).to eq(".")
+      expect(@cipher.key.length).to be = 6
+    end
+    it "can return a hash with 4 integer values from key" do
+      expect(@cipher.get_key_hash).to be_a Hash
+      expect(@cipher.get_key_hash.length).to eq(4)
+      expect(@cipher.get_key_hash[1]).to be_a Integer
+      expect(@cipher.get_key_hash[2]).to be_a Integer
+      expect(@cipher.get_key_hash[3]).to be_a Integer
+      expect(@cipher.get_key_hash[4]).to be_a Integer
     end
 end
