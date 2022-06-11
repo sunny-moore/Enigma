846a315763fe92bd29930978e1fd9e64579d2bc0
Sunny Moore <moore.sunny@gmail.com>
2022-06-11 13:20:43 -0700

    Test character set created in initialize

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,9 @@ describe Enigma do
     it "exists" do
       expect(@cipher).to be_a Enigma
     end
+    it "has a stored character set" do
+      expect(@cipher.char_set).to be_a Array
+      expect(@cipher.char_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+        "x", "y", "z", " "])
+    end
 end
