ec70e0be928342e50b1b7affbb443c97b2bc5962
Sunny Moore <moore.sunny@gmail.com>
2022-06-11 13:42:12 -0700

    Create random_number method to return a decimal rounded to 6 places

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,8 +3,13 @@ require 'date'
 
 class Enigma
   attr_reader :char_set
-  
+
   def initialize()
     @char_set = ("a".."z").to_a << " "
+  end
+  def random_number()
+    num = rand().round(6)
+    # binding.pry
+    num
   end
 end
