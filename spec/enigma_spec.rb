cd5b530aa0a71c90352c8772422ef46fce255e9f
Sunny Moore <moore.sunny@gmail.com>
2022-06-11 13:41:40 -0700

    Test random number generates a decimal rounded to 6 places

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -14,4 +14,12 @@ describe Enigma do
       expect(@cipher.char_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
         "x", "y", "z", " "])
     end
+    it "can generate a random decimal float ex: 0.012345" do
+      #There should be 6 numbers after the decimal to allow for a leading or trailing zero
+      expect(@cipher.random_number).to be_a Float
+      expect(@cipher.random_number).to be < 1
+      expect(@cipher.random_number).to be > 0
+      expect(@cipher.random_number.to_s.length).to be <= 8
+      expect(@cipher.random_number.to_s.length).to be >=7
+    end
 end
