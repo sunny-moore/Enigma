19a3131d7008899565d5b4c4db017f73ef1d50ae
Sunny Moore <moore.sunny@gmail.com>
2022-06-12 13:00:44 -0700

    Create encryptor class (replaced encrypt)

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,5 +1,7 @@
 require 'pry'
 require 'date'
+require './lib/enigma'
+
 class Encryptor
   attr_reader :enigma,
               :incoming_text,
@@ -13,19 +15,24 @@ class Encryptor
     @encrypt_result = Hash.new
   end
 
-  def file_open(filename = ARGV[0])
+  def start
+    file_open(filename = ARGV[0])
+    encrypt(key = "02715", date = "040895")
+    file_write(filename = ARGV[1])
+  end
+  def file_open(filename)
     # binding.pry
     handler = File.open(filename, "r")
     @incoming_text = handler.read
     handler.close
   end
 
-  def encrypt(key = ".00000", date)
+  def encrypt(key, date)
     @encrypt_result = @enigma.encrypt(@incoming_text, key, date)
     @outgoing_text = @encrypt_result[:encryption]
   end
 
-  def file_write(filename = ARGV[1])
+  def file_write(filename)
     writer = File.open(filename, "w")
     writer.write(@outgoing_text)
     writer.close
