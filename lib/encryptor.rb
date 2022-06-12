e4fa073ad74cb19b0cf1ce786d9d282cddd911ba
Sunny Moore <moore.sunny@gmail.com>
2022-06-12 12:32:13 -0700

    Moved code from encrypt.rb to encryptor.rb

---
+++ lib/encryptor.rb
@@ -0,0 +1,33 @@
+require 'pry'
+require 'date'
+class Encryptor
+  attr_reader :enigma,
+              :incoming_text,
+              :outgoing_text,
+              :encrypt_result
+
+  def initialize()
+    @enigma = Enigma.new
+    @incoming_text = ""
+    @outgoing_text = ""
+    @encrypt_result = Hash.new
+  end
+
+  def file_open(filename = ARGV[0])
+    # binding.pry
+    handler = File.open(filename, "r")
+    @incoming_text = handler.read
+    handler.close
+  end
+
+  def encrypt(key = ".00000", date)
+    @encrypt_result = @enigma.encrypt(@incoming_text, key, date)
+    @outgoing_text = @encrypt_result[:encryption]
+  end
+
+  def file_write(filename = ARGV[1])
+    writer = File.open(filename, "w")
+    writer.write(@outgoing_text)
+    writer.close
+  end
+end
