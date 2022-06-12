431a441a72a002a0268c9af828d4ad6704cdb331
Sunny Moore <moore.sunny@gmail.com>
2022-06-12 12:10:01 -0700

    Need to refactor, this should only be a runner.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -12,16 +12,22 @@ class Encrypt
     @incoming_text = ""
     @outgoing_text = ""
     @encrypt_result = Hash.new
+
   end
+
+
   def file_open(filename = ARGV[0])
+    # binding.pry
     handler = File.open(filename, "r")
     @incoming_text = handler.read
     handler.close
   end
+
   def encryptor(key = ".00000", date)
     @encrypt_result = @enigma.encrypt(@incoming_text, key, date)
     @outgoing_text = @encrypt_result[:encryption]
   end
+
   def file_write(filename = ARGV[1])
     writer = File.open(filename, "w")
     writer.write(@outgoing_text)
