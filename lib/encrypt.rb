3ef9eb8c718f80799744cdbfce4afb24a8db286a
Sunny Moore <moore.sunny@gmail.com>
2022-06-12 10:08:36 -0700

    Add empty outgoing_text variable to initialize

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,11 +2,12 @@ require 'pry'
 require 'date'
 
 class Encrypt
-  attr_reader :enigma, :incoming_text
+  attr_reader :enigma, :incoming_text, :outgoing_text
 
   def initialize()
     @enigma = Enigma.new
     @incoming_text = ""
+    @outgoing_text = ""
   end
   def file_open(filename = ARGV[0])
     handler = File.open(filename, "r")
