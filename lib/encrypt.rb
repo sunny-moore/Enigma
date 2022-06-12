2589205c9ac7a40d2b63c2c44636aca6916fd0a9
Sunny Moore <moore.sunny@gmail.com>
2022-06-12 10:39:02 -0700

    Add encrypt_result hash to initialize

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,16 +2,23 @@ require 'pry'
 require 'date'
 
 class Encrypt
-  attr_reader :enigma, :incoming_text, :outgoing_text
+  attr_reader :enigma,
+              :incoming_text,
+              :outgoing_text,
+              :encrypt_result
 
   def initialize()
     @enigma = Enigma.new
     @incoming_text = ""
     @outgoing_text = ""
+    @encrypt_result = Hash.new
   end
   def file_open(filename = ARGV[0])
     handler = File.open(filename, "r")
     @incoming_text = handler.read
     handler.close
+  end
+  def encryptor
+    @outgoing_text = @enigma.encrypt(@incoming_text)[:encryption]
   end
 end
