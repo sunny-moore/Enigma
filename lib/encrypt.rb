2ceda80338dda958a86b46c5082feb55f10eadfb
Sunny Moore <moore.sunny@gmail.com>
2022-06-12 09:56:17 -0700

    Add incoming_text to initialize, set at

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,9 +2,15 @@ require 'pry'
 require 'date'
 
 class Encrypt
-  attr_reader :enigma
-  
+  attr_reader :enigma, :incoming_text
+
   def initialize()
     @enigma = Enigma.new
+    @incoming_text = ""
+  end
+  def file_open
+    handler = File.open(ARGV[0], "r")
+    @incoming_text = handler.read
+    handler.close
   end
 end
