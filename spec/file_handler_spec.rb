1757c8ed79160320ad589ca4d5c621e28f999d54
Sunny Moore <moore.sunny@gmail.com>
2022-06-12 14:17:03 -0700

    Test that it creates encrypted.txt with expected string

--- spec/file_handler_spec.rb
+++ spec/file_handler_spec.rb
@@ -9,5 +9,19 @@ describe FileHandler do
     expect(expected).to eq("hello world\n")
     expect(File.open("message.txt").count).to eq(1)
   end
+  it "can write to a file and return file contents" do
+    expected = FileHandler.write("encrypted.txt", "keder ohulw\n")
+    expect(expected).to eq("keder ohulw\n")
+    expect(File.readable?("encrypted.txt")).to eq(true)
+  end
+  it "creates encrypted.txt file with encrypted message" do
+    #Tests that an actual file was created with expected msg
+    handler = File.open("encrypted.txt", "r")
+    expected = handler.read
+    handler.close
 
+    expect(expected).to be_a String
+    expect(expected.length).to eq(12)
+    expect(expected).to eq("keder ohulw\n")
+  end
 end
